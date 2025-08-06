73e217551dc332959553b8c7e40c64d4b6497190
spyke79 <167540765+spyke79@users.noreply.github.com>
2025-08-06 09:12:48 +0200

    for

--- src/inizio/Lezione_07_Esercizi_FOR.java
+++ src/inizio/Lezione_07_Esercizi_FOR.java
@@ -27,9 +27,28 @@ public class Lezione_07_Esercizi_FOR {
         /*
          * Chiedere all'utente di inserire 5 numeri
          * 
-         * Al termine dire qunti numeri pari sono stai inseriti e qunati numeri dispari
+         * Al termine dire quanti numeri pari sono stai inseriti e qunati numeri dispari
          * 
          */
+        
+        //Setto le variabili dell'esercizio
+        int n_es2, n_pari = 0, n_dispari = 0; //Setto anche i contatori dei numeri pari e dispari a 0
+        
+        System.out.println("Esercizio2: Inserisci 5 numeri e ti dirò quanti sono pari e quanti sono dispari");
+        
+        for(int i = 1; i <= 5; i++) {
+        	System.out.println("inserisci il " + i + "° numero");
+        	n_es2 = input.nextInt(); //Prendo il numero inserito nel teminale
+        	
+        	//Controllo se il numero inserito è pari o dispari
+        	if(n_es2%2 == 0) {
+        		n_pari++; //Incremento il contatore dei numeri pari
+        	}else {
+        		n_dispari++; //Incremento il contatore dei numeri dispari
+        	}
+        }
+        
+        System.out.println("Hai inserito " + n_pari + " numeri pari e " + n_dispari + " numeri dispari.");
         
         input.close();
 	}
