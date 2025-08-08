5900a09e631ca82379e4fa1e578ae161bc7bfaab
spyke79 <167540765+spyke79@users.noreply.github.com>
2025-08-08 17:57:59 +0200

    es

--- src/inizio/Esercizi_1.java
+++ src/inizio/Esercizi_1.java
@@ -13,6 +13,14 @@ public class Esercizi_1 {
 		
 		System.out.println("Il pc ha giocato: " + simboli[rm.nextInt(3)]); //Genera un numero casuale tra 0 e 3 esculo
 
+		/*
+	    Gioca alla morra cinese con il PC
+	    Il sistema esce quando viene scritto fine
+	    L'utente deve inserire Carta, Forbici o Sasso (non importa minuscole o maiscole)
+	    Quando faccio un lancio devo conoscere l'esito (Vinto, Perso, Paraggio)
+	    Quado finisco il gioco devo conoscere quate partite ho fatto e quante volte ho vinto perso o pareggiato
+	     */
+		
 	}
 
 }
